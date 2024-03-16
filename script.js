bb412b9f51c0d39fad757224e842b1ad05f4b69b
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-03-16 11:44:16 -0700

    implemented page refresh

--- script.js
+++ script.js
@@ -12,19 +12,25 @@ document.querySelector(`.score`).textContent = 10;
 document.querySelector(`.guess`).value = 23;
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(`.number`).textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector(`.message`).textContent = `No number!🚫`;
+
+    // WHen player wins
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `Correct Number!🎉`;
+    document.querySelector(`.number`).textContent = secretNumber;
+    document.querySelector(`body`).style.backgroundColor = `#60b347`;
+    document.querySelector(`.number`).style.width = `30rem`;
+
+    //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `Too high!`;
@@ -34,6 +40,8 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.message`).textContent = `You lose!💥`;
       document.querySelector(`.score`).textContent = 0;
     }
+
+    //when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(`.message`).textContent = `Too low!`;
@@ -44,4 +52,15 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.score`).textContent = 0;
     }
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(`.message`).textContent = `Start guessing...`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.guess`).value = ``;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  document.querySelector(`.number`).style.width = `15rem`;
 });
